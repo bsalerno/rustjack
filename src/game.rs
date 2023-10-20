8892878efc67a2140ea5eee4e26617cb4aae42de
Brian Salerno <Brian.Salerno@SYM-MBP-MQJWD0CX3K.local>
2023-10-20 12:59:39 -0600

    allow double down, move player instantiation to game mod

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
-use crate::cards::{Card, Hand, Rank};
+use crate::cards::{Card, Hand, Rank, Shoe};
+use std::io;
 
 pub struct Player {
     pub hand: Hand,
@@ -58,5 +59,89 @@ impl Player {
             aces -= 1;
         }
         score
+    }
+}
+
+pub fn play_hand(mut shoe: Shoe) -> f32 {
+    //instantiate players
+    let mut player = Player::new();
+    let mut dealer = Player::new();
+
+    // set initial hand multiplier to 1
+    // this will increase to 2 if doubled down
+    let mut multiplier: f32 = 1.0;
+    // deal initial cards
+    player.add_card(shoe.deal().unwrap());
+    dealer.add_card(shoe.deal().unwrap());
+    player.add_card(shoe.deal().unwrap());
+    dealer.add_card(shoe.deal().unwrap());
+
+    println!("Hand: {}", player.hand);
+    println!("Score: {:?}", player.score());
+
+    if player.score() == 21 {
+        println!("Blackjack!");
+        return 1.5;
+    }
+
+    // print dealer's up card
+    println!("Dealer: {}", dealer.hand.cards[0]);
+
+    //player's turn
+    loop {
+        println!("hit (h), stand (s), or double down (d)?");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        match input.trim() {
+            "h" => {
+                player.add_card(shoe.deal().unwrap());
+                println!("Hand: {}", player.hand);
+                println!("Score: {:?}", player.score());
+                if player.score() > 21 {
+                    println!("Busted.");
+                    return -1.0;
+                }
+            }
+            "d" => {
+                multiplier = 2.0;
+                player.add_card(shoe.deal().unwrap());
+                println!("Hand: {}", player.hand);
+                println!("Score: {:?}", player.score());
+                if player.score() > 21 {
+                    println!("Busted.");
+                    return -2.0;
+                }
+                // you can only get one card when doubling down
+                break;
+            }
+            "s" => {
+                break;
+            }
+            _ => {
+                println!("Invalid choice, hit (h) or stand (s).");
+            }
+        }
+    }
+
+    // dealer's turn
+    println!("Dealer: {}", dealer.hand);
+    while dealer.score() < 17 {
+        dealer.add_card(shoe.deal().unwrap());
+        println!("Dealer: {}", dealer.hand);
+    }
+    println!("Dealer score: {}", dealer.score());
+
+    if dealer.score() > 21 || player.score() > dealer.score() {
+        println!("Player wins!");
+        return 1.0 * multiplier;
+    } else if dealer.score() > player.score() {
+        println!("Dealer wins!");
+        return -1.0 * multiplier;
+    } else {
+        println!("Push!");
+        return 0.0;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,75 +2,17 @@ mod cards;
 mod game;
 
 use cards::Shoe;
-use game::Player;
-use std::io;
+use game::play_hand;
 
 fn main() {
     println!("yeah buddy");
 
+    let mut plusminus: f32 = 0.0;
     let mut shoe = Shoe::new(4); //example 4 deck shoe
-                                 // shuffle the shoe
+
+    // shuffle the shoe
     shoe.shuffle();
 
-    //instantiate players
-    let mut player = Player::new();
-    let mut dealer = Player::new();
-
-    // deal initial cards
-    player.add_card(shoe.deal().unwrap());
-    dealer.add_card(shoe.deal().unwrap());
-    player.add_card(shoe.deal().unwrap());
-    dealer.add_card(shoe.deal().unwrap());
-
-    println!("Hand: {}", player.hand);
-    println!("Score: {:?}", player.score());
-
-    if player.score() == 21 {
-        println!("Blackjack!");
-        return;
-    }
-
-    println!("Dealer: {}", dealer.hand.cards[0]);
-
-    //player's turn
-    loop {
-        println!("hit (h) or stand (s)?");
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-
-        match input.trim() {
-            "h" => {
-                player.add_card(shoe.deal().unwrap());
-                println!("Hand: {}", player.hand);
-                println!("Score: {:?}", player.score());
-                if player.score() > 21 {
-                    println!("Busted.");
-                    return;
-                }
-            }
-            "s" => {
-                break;
-            }
-            _ => {
-                println!("Invalid choice, hit (h) or stand (s).");
-            }
-        }
-    }
-
-    // dealer's turn
-    println!("Dealer: {}", dealer.hand);
-    while dealer.score() < 17 {
-        dealer.add_card(shoe.deal().unwrap());
-        println!("Dealer: {}", dealer.hand);
-    }
-
-    if dealer.score() > 21 || player.score() > dealer.score() {
-        println!("Player wins!");
-    } else if dealer.score() > player.score() {
-        println!("Dealer wins!");
-    } else {
-        println!("Push!");
-    }
+    plusminus += play_hand(shoe);
+    println!("Current +/-: {} units", plusminus);
 }
